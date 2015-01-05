b927645e4843ca7e23c453086cb8ed429e42182d
Filip Blaha <filip.blaha@hp.com>
2015-01-05 16:50:38 +0100

    functional test on policy enforcement

--- murano/tests/functional/engine/base.py
+++ murano/tests/functional/engine/base.py
@@ -17,8 +17,14 @@ import socket
 import time
 import uuid
 
+import keystoneclient
+import keystoneclient.openstack.common.apiclient.exceptions \
+    as keystone_exceptions
 import testresources
 import testtools
+
+import congressclient.v1.client as cclient
+import contextlib
 
 from heatclient import client as heatclient
 from keystoneclient.v2_0 import client as ksclient
@@ -27,6 +33,19 @@ import muranoclient.common.exceptions as exceptions
 
 import murano.tests.functional.engine.config as cfg
 
+INVALID_FLAVOR_RULE2 = {"rule": 'invalid_flavor_name("m1.small")'}
+
+INVALID_FLAVOR_RULE1 = {"rule":
+                        'is_valid_model(obj_id) :- '
+                        'murano_property(obj_id, "flavor", prop_value), '
+                        'invalid_flavor_name(prop_value)'}
+
+@contextlib.contextmanager
+def ignored(*exceptions):
+    try:
+        yield
+    except exceptions:
+        pass
 
 CONF = cfg.cfg.CONF
 
@@ -44,6 +63,15 @@ class MuranoBase(testtools.TestCase, testtools.testcase.WithAttributes,
                                           password=CONF.murano.password,
                                           tenant_name=CONF.murano.tenant,
                                           auth_url=CONF.murano.auth_url)
+
+        auth = keystoneclient.auth.identity.v2.Password(
+            auth_url=CONF.murano.auth_url,
+            username=CONF.murano.user,
+            password=CONF.murano.password,
+            tenant_name=CONF.murano.tenant)
+        session = keystoneclient.session.Session(auth=auth)
+        cls.congress_client = cclient.Client(session=session,
+                                             service_type='policy')
 
         heat_url = keystone_client.service_catalog.url_for(
             service_type='orchestration', endpoint_type='publicURL')
@@ -70,8 +98,8 @@ class MuranoBase(testtools.TestCase, testtools.testcase.WithAttributes,
         def upload_package(package_name, body, app):
 
             files = {'%s' % package_name: open(app, 'rb')}
-
-            return cls.muranoclient.packages.create(body, files)
+            with ignored(exceptions.HTTPInternalServerError):
+                return cls.muranoclient.packages.create(body, files)
 
         upload_package(
             'PostgreSQL',
@@ -103,10 +131,23 @@ class MuranoBase(testtools.TestCase, testtools.testcase.WithAttributes,
     def setUp(self):
         super(MuranoBase, self).setUp()
 
+        self.rules = []
+        with ignored(keystone_exceptions.Conflict):
+            self.rules.append(self.congress_client.create_policy_rule(
+                'classification',
+                INVALID_FLAVOR_RULE1))
+            self.rules.append(self.congress_client.create_policy_rule(
+                'classification',
+                INVALID_FLAVOR_RULE2))
+
         self.environments = []
 
     def tearDown(self):
         super(MuranoBase, self).tearDown()
+
+        for rule in self.rules:
+            self.congress_client.delete_policy_rule(
+                "classification", rule["id"])
 
         for env in self.environments:
             try:
@@ -129,13 +170,16 @@ class MuranoBase(testtools.TestCase, testtools.testcase.WithAttributes,
 
     def wait_for_environment_deploy(self, environment):
         start_time = time.time()
+        status = environment.manager.get(environment.id).status
 
-        while environment.manager.get(environment.id).status != 'ready':
+        while status != 'ready':
+            if 'failure' in status:
+                raise ValueError(status)
             if time.time() - start_time > 1200:
                 self.fail(
                     'Environment deployment is not finished in 1200 seconds')
             time.sleep(5)
-
+            status = environment.manager.get(environment.id).status
         return environment.manager.get(environment.id)
 
     def check_port_access(self, ip, port):
@@ -189,6 +233,30 @@ class MuranoBase(testtools.TestCase, testtools.testcase.WithAttributes,
         env = self._quick_deploy(environment_name, post_body)
 
         self.deployment_success_check(env, 23)
+
+    def test_deploy_telnet_policy_fail(self):
+        post_body = {
+            "instance": {
+                "flavor": "m1.small",
+                "image": self.linux,
+                "assignFloatingIp": True,
+                "?": {
+                    "type": "io.murano.resources.LinuxMuranoInstance",
+                    "id": str(uuid.uuid4())
+                },
+                "name": "testMurano"
+            },
+            "name": "teMurano",
+            "?": {
+                "type": "io.murano.apps.linux.Telnet",
+                "id": str(uuid.uuid4())
+            }
+        }
+
+        environment_name = 'Telnetenv' + uuid.uuid4().hex[:5]
+        self.assertRaises(ValueError,
+                          self._quick_deploy,
+                          environment_name, post_body)
 
     def test_deploy_apache(self):
         post_body = {
