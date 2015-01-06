84cdee0f1fd4c907e9e56ab9ffda4f91091a427d
Filip Blaha <filip.blaha@hp.com>
2015-01-06 14:09:54 +0100

    functional test on policy enforcement - test cleanup

--- murano/tests/functional/engine/test_policy_enf.py
+++ murano/tests/functional/engine/test_policy_enf.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 Mirantis, Inc.
+# Copyright (c) 2015 OpenStack Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -11,31 +11,29 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
+import congressclient.v1.client as cclient
 import contextlib
-
-import uuid
-
 import keystoneclient
 import keystoneclient.openstack.common.apiclient.exceptions \
     as keystone_exceptions
-import congressclient.v1.client as cclient
-import testresources
 import testtools
 import time
-
-import base
-import murano.tests.functional.engine.config as cfg
+import uuid
 
 from keystoneclient.v2_0 import client as ksclient
 from muranoclient import client as mclient
+
+import murano.tests.functional.engine.config as cfg
 
 
 INVALID_FLAVOR_RULE2 = {"rule": 'invalid_flavor_name("m1.small")'}
 
 INVALID_FLAVOR_RULE1 = {"rule":
-                            'is_valid_model(obj_id) :- '
-                            'murano_property(obj_id, "flavor", prop_value), '
-                            'invalid_flavor_name(prop_value)'}
+                        'is_valid_model(obj_id) :- '
+                        'murano_property(obj_id, "flavor", prop_value), '
+                        'invalid_flavor_name(prop_value)'}
+
 
 @contextlib.contextmanager
 def ignored(*exceptions):
@@ -46,8 +44,9 @@ def ignored(*exceptions):
 
 CONF = cfg.cfg.CONF
 
-class PolicyEnforcement(testtools.TestCase, testtools.testcase.WithAttributes,
-                        testresources.ResourcedTestCase):
+
+class PolicyEnforcement(testtools.TestCase):
+
     @classmethod
     def setUpClass(cls):
         super(PolicyEnforcement, cls).setUpClass()
@@ -72,7 +71,6 @@ class PolicyEnforcement(testtools.TestCase, testtools.testcase.WithAttributes,
                                           endpoint=CONF.murano.murano_url,
                                           token=keystone_client.auth_token)
 
-
     def setUp(self):
         super(PolicyEnforcement, self).setUp()
 
@@ -85,7 +83,6 @@ class PolicyEnforcement(testtools.TestCase, testtools.testcase.WithAttributes,
                 'classification',
                 INVALID_FLAVOR_RULE2))
 
-
     def tearDown(self):
         super(PolicyEnforcement, self).tearDown()
 
@@ -93,37 +90,25 @@ class PolicyEnforcement(testtools.TestCase, testtools.testcase.WithAttributes,
             self.congress_client.delete_policy_rule(
                 "classification", rule["id"])
 
-    def wait_for_environment_deploy(self, environment):
+    def wait_for_failure(self, environment):
         start_time = time.time()
-        status = environment.manager.get(environment.id).status
-
-        while status != 'ready':
-            if 'failure' in status:
-                raise ValueError(status)
-            if time.time() - start_time > 1200:
-                self.fail(
-                    'Environment deployment is not finished in 1200 seconds')
+        while not 'failure' in environment.manager.get(environment.id).status:
+            if time.time() - start_time > 300:
+                self.fail('Deployment not finished in 300 seconds')
             time.sleep(5)
-            status = environment.manager.get(environment.id).status
-        return environment.manager.get(environment.id)
 
-
-
-    def _quick_deploy(self, name, *apps):
+    def _quick_deploy(self, name, app):
         environment = self.muranoclient.environments.create({'name': name})
 
         session = self.muranoclient.sessions.configure(environment.id)
 
-        for app in apps:
-            self.muranoclient.services.post(environment.id,
-                                            path='/',
-                                            data=app,
-                                            session_id=session.id)
+        self.muranoclient.services.post(environment.id,
+                                        path='/',
+                                        data=app,
+                                        session_id=session.id)
 
         self.muranoclient.sessions.deploy(environment.id, session.id)
-        return self.wait_for_environment_deploy(environment)
-
-
+        return environment
 
     def test_deploy_telnet_policy_fail(self):
         post_body = {
@@ -145,7 +130,5 @@ class PolicyEnforcement(testtools.TestCase, testtools.testcase.WithAttributes,
         }
 
         environment_name = 'Telnetenv' + uuid.uuid4().hex[:5]
-        self.assertRaises(ValueError,
-                          self._quick_deploy,
-                          environment_name, post_body)
-
+        env = self._quick_deploy(environment_name, post_body)
+        self.wait_for_failure(env)
